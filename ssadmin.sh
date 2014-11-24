15ea0c39e4115f53bbbbeee25904c8e1e7e6c62f
hellofwy <hellofwy@gmail.com>
2014-11-24 20:57:44 +0800

    添加、删除用户

--- ssadmin.sh
+++ ssadmin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+USER_FILE=ssusers
 
 add_rules () {
     PORT=$1;
@@ -20,6 +21,36 @@ list_rules () {
     iptables -vnx -L
 }
 
+add_user () {
+    PORT=$1
+    PWORD=$2
+    CMETHED=$3
+    TLIMIT=$4
+    if [ ! -e $USER_FILE ]; then
+        echo "\
+# 以空格、制表符分隔
+# 端口 密码 加密方式 流量限制
+# 2345 abcde  aes-256-cfb 10GiB" > $USER_FILE;
+    fi
+    cat $USER_FILE |
+    awk '
+    {
+        if($1=='$PORT') exit 1
+    }'
+    if [ $? -eq 0 ]; then
+        echo "\
+$PORT $PWORD $CMETHED $TLIMIT" >> $USER_FILE;
+    else
+        echo "用户已存在!"
+    fi
+}
+
+del_user () {
+    PORT=$1
+    if [ -e $USER_FILE ]; then
+        sed -i '/^\s*'$PORT'\s/ d' $USER_FILE
+    fi
+}
 case $1 in
     add )
         shift
@@ -31,6 +62,14 @@ case $1 in
         ;;
     list )
         list_rules
+        ;;
+    au )
+        shift
+        add_user $1 $2 $3 $4
+        ;;
+    du )
+        shift
+        del_user $1
         ;;
 esac
 
